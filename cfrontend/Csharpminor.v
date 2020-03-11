8237223c4de8b5e4aadf4a6dce32916f4d35b840
Jérémy Thibault <jeremy.thibault@inria.fr>
2020-03-11 15:43:32 +0100

    Add compartments to the external call trace events for Csharpminor

--- cfrontend/Csharpminor.v
+++ cfrontend/Csharpminor.v
@@ -161,6 +161,7 @@ Inductive state: Type :=
   | Callstate:                  (**r Invocation of a function *)
       forall (f: fundef)                (**r function to invoke *)
              (args: list val)           (**r arguments provided by caller *)
+             (cp: compartment)          (**r current compartment *)
              (k: cont)                  (**r what to do next  *)
              (m: mem),                  (**r memory state *)
       state
@@ -317,6 +318,7 @@ Section EVAL_EXPR.
 Variable e: env.
 Variable le: temp_env.
 Variable m: mem.
+(* JT: TODO: Might probably need to add a compartment variable to this section *)
 
 Inductive eval_expr: expr -> val -> Prop :=
   | eval_Evar: forall id v,
@@ -385,17 +387,19 @@ Inductive step: state -> trace -> state -> Prop :=
       step (State f (Sstore chunk addr a) k e le m)
         E0 (State f Sskip k e le m')
 
-  | step_call: forall f optid sig a bl k e le m vf vargs fd,
+  | step_call: forall f cp optid sig a bl k e le m vf vargs fd,
+      cp = f.(fn_comp) ->
       eval_expr e le m a vf ->
       eval_exprlist e le m bl vargs ->
       Genv.find_funct ge vf = Some fd ->
       funsig fd = sig ->
       step (State f (Scall optid sig a bl) k e le m)
-        E0 (Callstate fd vargs (Kcall optid f e le k) m)
+        E0 (Callstate fd vargs cp (Kcall optid f e le k) m)
 
-  | step_builtin: forall f optid ef bl k e le m vargs t vres m',
+  | step_builtin: forall f cp optid ef bl k e le m vargs t vres m',
+      cp = f.(fn_comp) ->
       eval_exprlist e le m bl vargs ->
-      external_call ef ge vargs m t vres m' ->
+      external_call ef ge cp vargs m t vres m' ->
       step (State f (Sbuiltin optid ef bl) k e le m)
          t (State f Sskip k e (Cminor.set_optvar optid vres le) m')
 
@@ -451,18 +455,18 @@ Inductive step: state -> trace -> state -> Prop :=
       step (State f (Sgoto lbl) k e le m)
         E0 (State f s' k' e le m)
 
-  | step_internal_function: forall f vargs k m m1 e le,
+  | step_internal_function: forall f cp vargs k m m1 e le,
       list_norepet (map fst f.(fn_vars)) ->
       list_norepet f.(fn_params) ->
       list_disjoint f.(fn_params) f.(fn_temps) ->
       alloc_variables empty_env m (fn_vars f) e m1 ->
       bind_parameters f.(fn_params) vargs (create_undef_temps f.(fn_temps)) = Some le ->
-      step (Callstate (Internal f) vargs k m)
+      step (Callstate (Internal f) vargs cp k m)
         E0 (State f f.(fn_body) k e le m1)
 
-  | step_external_function: forall ef vargs k m t vres m',
-      external_call ef ge vargs m t vres m' ->
-      step (Callstate (External ef) vargs k m)
+  | step_external_function: forall ef cp vargs k m t vres m',
+      external_call ef ge cp vargs m t vres m' ->
+      step (Callstate (External ef) vargs cp k m)
          t (Returnstate vres k m')
 
   | step_return: forall v optid f e le k m,
@@ -483,7 +487,7 @@ Inductive initial_state (p: program): state -> Prop :=
       Genv.find_symbol ge p.(prog_main) = Some b ->
       Genv.find_funct_ptr ge b = Some f ->
       funsig f = signature_main ->
-      initial_state p (Callstate f nil Kstop m0).
+      initial_state p (Callstate f nil default_compartment Kstop m0).
 
 (** A final state is a [Returnstate] with an empty continuation. *)
 
