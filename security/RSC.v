6ce60a8eea033dd6a4e08f5782c419ce59a5242f
Jérémy Thibault <jeremy.thibault@mpi-sp.org>
2023-02-09 15:52:35 +0100

    Attempt to define unlinking

--- security/RSC.v
+++ security/RSC.v
@@ -5,10 +5,96 @@ Require Import AST Linking Smallstep Events Behaviors.
 Require Import Csyntax Asm.
 Require Import Compiler Complements.
 
+Section Split.
+
+  Variant side := Left | Right.
+
+  Definition split := compartment -> side.
+
+  Fixpoint unlink_prog_defs_left (s: split) (defs: list (ident * globdef (Ctypes.fundef Csyntax.function) Ctypes.type)):
+    list (ident * globdef (Ctypes.fundef Csyntax.function) Ctypes.type) :=
+    match defs with
+    | nil => nil
+    | (id, d) :: defs' =>
+        match d with
+        | Gfun (Ctypes.Internal f) =>
+            match s (Csyntax.fn_comp f) with
+            | Left => (id, Gfun (Ctypes.Internal f)) :: (unlink_prog_defs_left s defs')
+            | Right => (id, Gfun (Ctypes.External (EF_external ""
+                                                    (Csyntax.fn_comp f)
+                                                    {| sig_args := map (fun '(x, ty) => Ctypes.typ_of_type ty) (fn_params f);
+                                                      sig_res := Ctypes.typ_of_type (fn_return f);
+                                                      sig_cc := fn_callconv f|})
+                      (Ctypes.type_of_params (Csyntax.fn_params f)) (fn_return f) (fn_callconv f))) ::
+                        (unlink_prog_defs_left s defs')
+            end
+        | Gfun (Ctypes.External f tys ty cc) => (id, Gfun (Ctypes.External f tys ty cc)) :: (unlink_prog_defs_left s defs')
+        | Gvar v => (id, Gvar v) :: (unlink_prog_defs_left s defs')
+        end
+    end
+    .
+
+  Fixpoint unlink_prog_defs_right (s: split) (defs: list (ident * globdef (Ctypes.fundef Csyntax.function) Ctypes.type)):
+    list (ident * globdef (Ctypes.fundef Csyntax.function) Ctypes.type) :=
+    match defs with
+    | nil => nil
+    | (id, d) :: defs' =>
+        match d with
+        | Gfun (Ctypes.Internal f) =>
+            match s (Csyntax.fn_comp f) with
+            | Right => (id, Gfun (Ctypes.Internal f)) :: (unlink_prog_defs_left s defs')
+            | Left => (id, Gfun (Ctypes.External (EF_external ""
+                                                    (Csyntax.fn_comp f)
+                                                    {| sig_args := map (fun '(x, ty) => Ctypes.typ_of_type ty) (fn_params f);
+                                                      sig_res := Ctypes.typ_of_type (fn_return f);
+                                                      sig_cc := fn_callconv f|})
+                      (Ctypes.type_of_params (Csyntax.fn_params f)) (fn_return f) (fn_callconv f))) ::
+                        (unlink_prog_defs_left s defs')
+            end
+        | Gfun (Ctypes.External f tys ty cc) => (id, Gfun (Ctypes.External f tys ty cc)) :: (unlink_prog_defs_left s defs')
+        | Gvar v => (id, Gvar v) :: (unlink_prog_defs_left s defs')
+        end
+    end
+    .
+
+  Definition unlink (s: split) (p: Csyntax.program): Csyntax.program * Csyntax.program :=
+    ({| Ctypes.prog_defs := unlink_prog_defs_left s (Ctypes.prog_defs p);
+       Ctypes.prog_public := Ctypes.prog_public p;
+       Ctypes.prog_main := Ctypes.prog_main p;
+       Ctypes.prog_pol := Ctypes.prog_pol p;
+       Ctypes.prog_types := Ctypes.prog_types p;
+       Ctypes.prog_comp_env := Ctypes.prog_comp_env p;
+       Ctypes.prog_comp_env_eq := Ctypes.prog_comp_env_eq p;
+    |},
+    {| Ctypes.prog_defs := unlink_prog_defs_right s (Ctypes.prog_defs p);
+       Ctypes.prog_public := Ctypes.prog_public p;
+       Ctypes.prog_main := Ctypes.prog_main p;
+       Ctypes.prog_pol := Ctypes.prog_pol p;
+       Ctypes.prog_types := Ctypes.prog_types p;
+       Ctypes.prog_comp_env := Ctypes.prog_comp_env p;
+       Ctypes.prog_comp_env_eq := Ctypes.prog_comp_env_eq p;
+    |}).
+
+    Lemma link_unlink (s: split):
+      forall p p1 p2,
+        unlink s p = (p1, p2) ->
+        link p1 p2 = Some p.
+    Proof.
+      intros.
+      Local Transparent Ctypes.Linker_program Ctypes.link_program Linker_prog.
+      unfold link, Ctypes.Linker_program, Ctypes.link_program, link, Linker_prog; simpl.
+      rewrite link_prog_succeeds; simpl.
+      - destruct p1, p2; inv H. simpl. admit.
+      - destruct p1, p2; inv H; reflexivity.
+      - destruct p1, p2; inv H. unfold prog_defmap; simpl.
+        admit.
+      - destruct p1, p2; inv H. simpl. now rewrite Policy.eqb_refl.
+    Admitted.
 
 Section RSC.
 
   Variable pol: Policy.t.
+  Variable s: split.
 
   Variable p: Csyntax.program.
   Hypothesis pol_p: Ctypes.prog_pol p = pol.
