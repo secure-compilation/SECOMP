52b8b1f52b2dc5e581e68bf0c08294e9755759df
Sven Argo <sven.argo@rub.de>
2023-11-03 15:42:49 +0100

    Generate (biased) random traces with QCheck

--- test/backtranslation/main.ml
+++ test/backtranslation/main.ml
@@ -10,13 +10,43 @@ let test =
 (* we can check right now the property... *)
 let _ = QCheck_runner.run_tests [test]
 
-(* QCheck generators for types *)
 
-let event_val () =
-  let n = QCheck.small_int in
-  let g = QCheck.get_gen n in
-  let rand_state = Random.get_state () in
-  g rand_state
+
+
+(* QCheck arbitrary "instances" for relevant base types *)
+
+let memory_chunk : AST.memory_chunk QCheck.arbitrary = QCheck.oneofl AST.[Mint8signed; Mint8unsigned; Mint16signed; Mint16unsigned; Mint32; Mint64; Mfloat32; Mfloat64; Many32; Many64]
+
+let positive : BinNums.positive QCheck.arbitrary = QCheck.(map (fun i -> Camlcoq.P.of_int i) small_int)
+
+let coq_Z : BinNums.coq_Z QCheck.arbitrary = QCheck.(map (fun i -> Camlcoq.Z.of_sint i) small_int)
+
+let ident : AST.ident QCheck.arbitrary = positive
+
+let compartment : AST.compartment QCheck.arbitrary = positive
+
+let ptrofs : Integers.Ptrofs.int QCheck.arbitrary = QCheck.map (fun i -> Integers.Ptrofs.of_int i) coq_Z
+
+let name : (char list) QCheck.arbitrary = QCheck.(small_list printable_char)
+
+let binary_float : Floats.float QCheck.arbitrary =
+  let open QCheck in
+  let open Binary in
+  let zero = map (fun b -> B754_zero b) bool in
+  let infinity = map (fun b -> B754_infinity b) bool in
+  let nan = map (fun (b, p) -> B754_nan (b, p)) (pair bool positive) in
+  let finite = map (fun (b, p, z) -> B754_finite (b, p, z)) (triple bool positive coq_Z) in
+  oneof [zero; infinity; nan; finite]
+
+let eventval : Events.eventval QCheck.arbitrary =
+  let open QCheck in
+  let open Events in
+  let evint = map (fun i -> EVint (Camlcoq.Z.of_sint i)) small_int in
+  let evlong = map (fun i -> EVlong (Camlcoq.Z.of_sint i)) small_int in
+  let evfloat = map (fun f -> EVfloat f) binary_float in
+  let evsingle = map (fun f -> EVfloat f) binary_float in
+  let evptr_global = map (fun (i, p) -> EVptr_global (i, p)) (pair ident ptrofs) in
+  oneof [evint; evlong; evfloat; evsingle; evptr_global]
 
 let mk_syscall () =
   let name = [] in
@@ -56,15 +86,29 @@ let mk_return () =
   let ret_val = Events.EVint (Camlcoq.Z.of_uint 3) in
   Events.Event_return (src_compartment, trgt_compartment, ret_val)
 
+let trace rand_state =
+  let open QCheck.Gen in
+  let len = small_nat rand_state in
+  let rec trace_aux = function
+  | 0 -> []
+  | n ->
+    let f = float_range 0.0 1.0 rand_state in
+    if f < 0.6
+    then mk_call () :: (List.append (trace_aux (n-1)) [mk_return ()])
+    else mk_syscall () :: trace_aux (n-1) in
+  trace_aux len
+
+let () = print_endline "hah"
+
+let event_to_string e =
+  ignore (Format.flush_str_formatter ());
+  Interp.print_event Format.str_formatter e;
+  Format.flush_str_formatter ()
+
 let () =
-  let sys_call = mk_syscall () in
-  let vload = mk_vload () in
-  let vstore = mk_vstore () in
-  let annot = mk_annot () in
-  let call = mk_call () in
-  let ret = mk_return () in
-  let print e = Interp.print_event Format.std_formatter e in
-  List.iter (fun e -> print e; Printf.printf "\n") [sys_call; vload; vstore; annot; call; ret]
+  let rand_state = Random.get_state () in
+  let t = trace rand_state in
+  print_endline (String.concat "\n" (List.map event_to_string t))
 
 (*
    type ident = positive
