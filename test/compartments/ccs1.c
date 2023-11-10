aa0fe98e004c8ba2c2e6e15dd3a8db311554eff8
Catalin Hritcu <catalin.hritcu@gmail.com>
2023-11-10 17:26:21 +0100

    Made ccs1.c a bit more realistic

--- test/compartments/ccs1.c
+++ test/compartments/ccs1.c
@@ -1,73 +1,65 @@
 #include <stdio.h>
 
-/* environment */
-
-§E§ » read
-§E§ » write
-
-§E§ int read() {
-  return 0;
-}
-
-§E§ int write(int data, int x) {
-  return 0;
-}
-
 /* component C0 */
 
 §C0§ » valid
 
 §C0§ int valid(int data) {
-  return 1;
+  return data % 2;
 }
 
 /* component C2 */
 
-§C2§ « §E§[write]
+/* §C2§ imports_syscall [printf] */
 §C2§ « §C0§[valid]
 §C2§ » init
 §C2§ » process
 
+§C2§ int initialized = 0;
+
 §C2§ int init() {
+  initialized = 1;
   return 0;
 }
 
 // can yield Undef if not initialized
 §C2§ int prepare() {
-  return 0;
+  return 77 / initialized;
 }
 
 // can yield Undef for some y
 §C2§ int handle(int y) {
-  return 0;
+  return (y + 1) / y;
 }
 
-§C2§ int process(int x, int y) {
+§C2§ int process(int y) {
   int data;
   prepare();
   data = handle(y);
-  if (valid(data)) {
-    write(data, x);
-  }
+  if (valid(data)) { printf("%d,%d\n",data,y); }
+  else { printf("invalid data\n"); }
   return 0;
 }
 
 /* component C1 */
 
-§C1§ « §E§[read]
+/* §C1§ imports_syscall [fgets] */
 §C1§ « §C2§[init]
 §C1§ « §C2§[process]
 
 // can yield Undef for some x
-§C1§ int parse(int x) {
-  return x;
+§C1§ int parse(char* x) {
+  if (x[0]) return 42; /* <- should be atoi(x) */
+  else return 0;
 }
 
+§C1§ char x[10];
+
 §C1§ int main() {
-  int x, y;
   init();
-  x = read();
-  y = parse(x);
-  process(x, y);
-  return 0;
+  if (fgets(x,10,stdin) != NULL) {
+    int y = parse(x);
+    process(y);
+    return 0;
+  }
 }
