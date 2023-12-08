cdd8a9602c65bccd2ad726de47b1fd5090a76fc0
Sven Argo <sven.argo@rub.de>
2023-12-08 18:01:39 +0100

    Fix special floating point values and missing includes for generated C code

--- test/backtranslation/test_backtranslation.ml
+++ test/backtranslation/test_backtranslation.ml
@@ -1,15 +1,35 @@
+let rename_special_floating_point_values code =
+  let r_inf = Str.regexp "inf" in
+  let r_nan = Str.regexp "nan" in
+  code
+  |> Str.global_replace r_inf "INFINITY"
+  |> Str.global_replace r_nan "NAN"
+
+let rename_main main code =
+  let regex_main = Str.regexp ("\\$" ^ string_of_int main ^ "(") in
+  Str.global_replace regex_main "main(" code
+
+let rename_funcs code =
+  let regex = Str.regexp "\\$\\([0-9]+\\)" in
+  Str.global_replace regex "ident_\\1" code
+
+let prepend_header code =
+  "#include <math.h>\n" ^ code
+
 let export_c_light_program prog main file_name =
   let version = PrintClight.Clight1 in
-  let code =
+  let raw_code =
     ignore (Format.flush_str_formatter ());
     PrintClight.print_program version Format.str_formatter prog;
     Format.flush_str_formatter () in
-  Out_channel.with_open_text (file_name ^ ".raw") (fun c -> output_string c code);
-  let regex_main = Str.regexp ("\\$" ^ string_of_int main ^ "(") in
-  let code_with_main = Str.global_replace regex_main "main(" code in
-  let regex = Str.regexp "\\$\\([0-9]+\\)" in
-  let clean_code = Str.global_replace regex "ident_\\1" code_with_main in
-  Out_channel.with_open_text file_name (fun c -> output_string c clean_code)
+  Out_channel.with_open_text (file_name ^ ".raw") (fun c -> output_string c raw_code);
+  let code =
+    raw_code
+    |> rename_main main
+    |> rename_funcs
+    |> rename_special_floating_point_values
+    |> prepend_header in
+  Out_channel.with_open_text file_name (fun c -> output_string c code)
 
 (* Run QCheck testing *)
 
