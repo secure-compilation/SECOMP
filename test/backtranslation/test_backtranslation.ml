c5c33233d2135473f4f4c288499bfb2ba38341d6
Sven Argo <sven.argo@rub.de>
2023-12-19 15:09:51 +0100

    Improve usability for multiple ASM progs/traces

--- test/backtranslation/test_backtranslation.ml
+++ test/backtranslation/test_backtranslation.ml
@@ -1,6 +1,5 @@
 (* QCheck testing *)
 let property_under_test asm_prog bundled_trace =
-  let () = print_endline (Show.show_bundle_trace bundled_trace) in
   let source_name = "out.c" in
   let ccomp_cmd = "../../ccomp -quiet -c > /dev/null 2> /dev/null" in
   let src_program = Backtranslation.gen_program bundled_trace asm_prog in
@@ -13,26 +12,30 @@ let property_under_test asm_prog bundled_trace =
 let bundle_trace ctx =
   QCheck.make ~print:Show.show_bundle_trace (Gen.bundle_trace ctx)
 
-let test_backtranslation asm_prog ctx =
-  QCheck.Test.make ~count:1 ~name:"backtranslation" (bundle_trace ctx)
+let test_backtranslation name asm_prog ctx =
+  QCheck.Test.make ~count:1 ~name:name (bundle_trace ctx)
     (property_under_test asm_prog)
 
 let parse_args () =
-  let usage_msg = "test_backtranslation [-seed n] [-verbose]" in
+  let usage_msg = "test_backtranslation [-seed n] [-verbose] [-num_traces n] [-num_asm_progs n]" in
   let seed = ref 0 in
   let debug = ref false in
+  let num_traces = ref 10 in
+  let num_asm_progs = ref 10 in
   let anon_fun _ = failwith "Unnamed arguments are not supported" in
   let speclist =
     [
       ("-seed", Arg.Set_int seed, "Initial random seed");
-      ("-debug", Arg.Set debug, "Provide debug output")
+      ("-debug", Arg.Set debug, "Provide debug output");
+      ("-num_traces", Arg.Set_int num_traces, "Set the number of traces tested per ASM program (default = 10)");
+      ("-num_asm_progs", Arg.Set_int num_asm_progs, "Set the number of ASM programs (default = 10)")
     ] in
   let () = Arg.parse speclist anon_fun usage_msg in
-  (!seed, !debug)
+  (!seed, !debug, !num_traces, !num_asm_progs)
 
 (* Main *)
 let _ =
-  let (seed, debug) = parse_args () in
+  let (seed, debug, num_traces, num_asm_progs) = parse_args () in
   let config =
     Gen_ctx.
       {
@@ -52,13 +55,30 @@ let _ =
     if seed = 0
     then
       (Random.self_init ();
-      let s = Random.full_int 10000 in
-      Printf.printf "seed = %d\n" s;
+      let s = Random.full_int (Int.shift_left 1 32) in
+      Printf.printf "Root seed = %d\n" s;
       Random.init s)
     else Random.init seed
   in
-  let rand_state = Random.get_state () in
-  let ctx = Gen_ctx.random config rand_state in
-  let asm_prog = Gen_ctx.get_asm_prog ctx in
-  if debug then PrintAsm.print_program_asm Out_channel.stdout asm_prog else ();
-  QCheck_runner.run_tests ~verbose:true ~rand:rand_state [ test_backtranslation asm_prog ctx ]
+  let discard_out = Out_channel.open_text "/dev/null" in
+  for i = 0 to num_asm_progs - 1 do
+    let bound = Int.shift_left 1 32 in
+    let asm_seed = Random.full_int bound in
+    let trace_root_seed = Random.full_int bound in
+    let () = Random.init asm_seed in
+    let rand_state = Random.get_state () in
+    let ctx = Gen_ctx.random config rand_state in
+    let asm_prog = Gen_ctx.get_asm_prog ctx in
+    let () = Printf.printf "Testing traces for ASM program (asm_seed = %d)\n" asm_seed in
+    for j = 0 to num_traces - 1 do
+      let () = Random.init (trace_root_seed + j) in
+      let trace_seed = Random.full_int bound in
+      let () = Random.init trace_seed in
+      let name = Printf.sprintf "\ttest_backtranslation (trace_seed = %d)" trace_seed in
+      if 0 = QCheck_runner.run_tests ~out:discard_out ~rand:rand_state [ test_backtranslation name asm_prog ctx ]
+      then Printf.printf "%s: passed\n" name
+      else Printf.printf "%s: failed\n" name;
+      Out_channel.flush Out_channel.stdout
+    done
+  done;
+  Out_channel.close discard_out
